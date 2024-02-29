301e98d8b9dc71828dda412cf4a623e687e2cf6a
eufrat <lukzyw@gmail.com>
2024-02-29 19:49:29 +0100

    Create shadow jar and Spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("java")
+//    id("org.springframework.boot") version "3.2.3"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
+
 
 group = "klg.backend.lukasz"
 version = "1.0-SNAPSHOT"
@@ -12,8 +15,16 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.3")
+//    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.3")
+
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "klg.backend.lukasz.Main"
 }
